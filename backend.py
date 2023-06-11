f3794186cd8e34e8083b358bb23b90ee5212f5a6
pg3328 <pg3328@rit.edu>
2023-06-11 17:11:42 -0400

    Rearranged the repository and pushing the code

---
+++ backend.py
@@ -0,0 +1,113 @@
+import dbconnection
+from flask import Flask, request, render_template
+
+client = dbconnection.get_the_connection()
+app = Flask(__name__)
+
+
+def query1(update_year,protocol,lower_rating,upper_rating,tags):
+    update_year='^'+update_year
+    query = {"dateModified": {"$regex": update_year}, "protocols": protocol, "rating": {"$gt": lower_rating, "$lt": upper_rating},
+             "tags": {"$in": tags}}
+    db = client["programmable_web"]
+    api_data = db["api_collection"]
+    result = api_data.find(query)
+    api_names = [doc['name'] for doc in result]
+    print(api_names)
+    return api_names
+
+
+def query2(update_year,lower_rating,upper_rating,tags,apis):
+    update_year = "^"+update_year
+    query = {"updated": {"$regex": update_year}, "rating": {"$gt": lower_rating, "$lt": upper_rating}, "tags": {"$in": tags},
+             "apis": {"$in": apis}}
+    db = client["programmable_web"]
+    api_data = db["mashup_collection"]
+    result = api_data.find(query)
+    mashup_names = [doc['name'] for doc in result]
+    return mashup_names
+
+
+def search_api_by_keywords(keywords):
+    client = dbconnection.get_the_connection()
+    db = client["programmable_web"]
+    api_data = db["api_collection"]
+
+    query = {"$and": []}
+    for keyword in keywords:
+        query["$and"].append({"$or": [
+            {"title": {"$regex": f".*{keyword}.*", "$options": "i"}},
+            {"summary": {"$regex": f".*{keyword}.*", "$options": "i"}},
+            {"description": {"$regex": f".*{keyword}.*", "$options": "i"}}
+        ]})
+
+    result = api_data.find(query)
+    api_names = [doc["name"] for doc in result]
+    return api_names
+
+
+def search_mashup_by_keywords(keywords):
+    client = dbconnection.get_the_connection()
+    db = client["programmable_web"]
+    mashup_data = db["mashup_collection"]
+    query = {"$and": []}
+    for keyword in keywords:
+        query["$and"].append({"$or": [
+            {"title": {"$regex": f".*{keyword}.*", "$options": "i"}},
+            {"summary": {"$regex": f".*{keyword}.*", "$options": "i"}},
+            {"description": {"$regex": f".*{keyword}.*", "$options": "i"}}
+        ]})
+
+    result = mashup_data.find(query)
+    mashup_names = [doc["name"] for doc in result]
+    return mashup_names
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    my_request = request.form.get("request")
+    print(my_request)
+    my_service = request.form.get("service")
+    print(my_service)
+    if my_request == "keyword":
+        my_keywords = request.form.get("keywords")
+        my_keywords = my_keywords.split(',')
+        print(my_keywords)
+        if my_service == "mashup":
+            print("a")
+            name = search_mashup_by_keywords(my_keywords)
+            print(name)
+            return render_template("index.html", names=name)
+        else:
+            name = search_api_by_keywords(my_keywords)
+            return render_template("index.html", names=name)
+    else:
+        my_updated_year = request.form.get("updated-year")
+        print(my_updated_year)
+        my_rating = request.form.get("rating")
+        print(my_rating)
+        my_rating = my_rating.split('-')
+        print(my_rating)
+        my_tags = request.form.get("tags")
+        my_tags = my_tags.split(',')
+        print(my_tags)
+        if my_service == "mashup":
+            my_apis = request.form.get("apis")
+            my_apis = my_apis.split(',')
+            name = query2(my_updated_year, float(my_rating[0]), float(my_rating[1]), my_tags, my_apis)
+            return render_template("index.html", names=name)
+        else:
+            my_protocol = request.form.get("protocols")
+            name = query1(my_updated_year, my_protocol, float(my_rating[0]), float(my_rating[1]), my_tags)
+            return render_template("index.html", names=name)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ dbconnection.py
@@ -0,0 +1,141 @@
+import pymongo
+from urllib.parse import quote_plus
+
+client_id = 'mongodb+srv://pg3328:room919@N@programmingweb.2jrqleo.mongodb.net/?retryWrites=true&w=majority'
+username = "pg3328"
+password = "room919@N"
+client_id = client_id.replace(username, quote_plus(username))
+client_id = client_id.replace(password, quote_plus(password))
+
+
+def insert_data(mycollection, data_to_be_inserted):
+    mycollection.insert_many(data_to_be_inserted)
+
+
+def read_mashup_data(filename):
+    """
+    Read the mashup data from the file
+    :param filename: The name of the file to read
+    :return: The JSON object containing the mashup data
+    """
+    with open(filename, 'r', encoding="ISO-8859-1") as f:
+        lines = f.readlines()
+
+    mashup_records = []
+
+    for line in lines:
+        fields = line.strip().split('$#$')
+        api_used=[]
+        api_links=[]
+        for apis in fields[16].split('###'):
+            api_split = apis.split('$$$')
+            api_used.append(api_split[0])
+
+        rating = float(fields[3]) if fields[3] else 0.0
+        mashup_record = {
+            'id': fields[0],
+            'title': fields[1],
+            'summary': fields[2],
+            'rating': rating,
+            'name': fields[4],
+            'label': fields[5],
+            'author': fields[6],
+            'description': fields[7],
+            'type': fields[8],
+            'downloads': fields[9],
+            'useCount': fields[10],
+            'sampleUrl': fields[11],
+            'dateModified': fields[12],
+            'numComments': fields[13],
+            'commentsUrl': fields[14],
+            'tags': fields[15].split('###'),
+            'apis': api_used,
+            'updated': fields[17]
+        }
+        mashup_records.append(mashup_record)
+    return mashup_records
+
+
+def read_api_data(filename):
+    """
+    Read the API data from the file
+    :param filename: The name of the file to read
+    :return: The JSON object containing the API data
+    """
+    with open(filename, 'r', encoding="ISO-8859-1") as f:
+        lines = f.readlines()
+
+    api_records = []
+
+
+    for line in lines:
+        fields = line.strip().split('$#$')
+        rating = float(fields[3]) if fields[3] else 0.0
+        api_record = {
+            'id': fields[0],
+            'title': fields[1],
+            'summary': fields[2],
+            'rating': rating,
+            'name': fields[4],
+            'label': fields[5],
+            'author': fields[6],
+            'description': fields[7],
+            'type': fields[8],
+            'downloads': fields[9],
+            'useCount': fields[10],
+            'sampleUrl': fields[11],
+            'downloadUrl': fields[12],
+            'dateModified': fields[13],
+            'remoteFeed': fields[14],
+            'numComments': fields[15],
+            'commentsUrl': fields[16],
+            'tags': fields[17].split('###'),
+            'category': fields[18],
+            'protocols': fields[19],
+            'serviceEndpoint': fields[20],
+            'version': fields[21],
+            'wsdl': fields[22],
+            'dataFormats': fields[23],
+            'apiGroups': fields[24],
+            'example': fields[25],
+            'clientInstall': fields[26],
+            'authentication': fields[27],
+            'ssl': fields[28],
+            'readonly': fields[29],
+            'VendorApiKits': fields[30],
+            'CommunityApiKits': fields[31],
+            'blog': fields[32],
+            'forum': fields[33],
+            'support': fields[34],
+            'accountReq': fields[35],
+            'commercial': fields[36],
+            'provider': fields[37],
+            'managedBy': fields[38],
+            'nonCommercial': fields[39],
+            'dataLicensing': fields[40],
+            'fees': fields[41],
+            'limits': fields[42],
+            'terms': fields[43],
+            'company': fields[44],
+            'updated': fields[45]
+        }
+
+        api_records.append(api_record)
+
+    return api_records
+
+
+def get_the_connection():
+    return pymongo.MongoClient(client_id)
+
+
+def main():
+    myclient = get_the_connection()
+    production = myclient.programmable_web
+    # api_collection = production.api_collection
+    # api_data = read_api_data("api.txt")
+    # insert_data(api_collection, api_data)
+    mashup_collection = production.mashup_collection
+    mashup_data = read_mashup_data("mashup.txt")
+    insert_data(mashup_collection, mashup_data)
+
